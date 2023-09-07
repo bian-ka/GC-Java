05be38438b377ccefae704869ed03d599b09c162
Bianca Gomes <gomes18bianca@gmail.com>
2023-09-06 22:58:22 -0300

    exercicio três

--- aula-01/src/main/java/org/example/exerciciodois.java
+++ aula-01/src/main/java/org/example/exerciciodois.java
@@ -1,2 +1,22 @@
-package org.example;public class exerciciodois {
+package org.example;
+
+import java.util.Scanner;
+
+public class exerciciodois {
+
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+        System.out.println("Digite um número: ");
+
+        int numero1 = scanner.nextInt();
+
+        System.out.println("Digite o segundo número: ");
+
+        int numero2 = scanner.nextInt();
+
+        System.out.println("Primeiro número: " + numero1);
+        System.out.println("Segundo número: " + numero2);
+    }
+
 }
