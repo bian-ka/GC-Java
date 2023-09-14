609b2bce34224a79e3736176436fc5e4349be1da
Bianca Gomes <gomes18bianca@gmail.com>
2023-09-14 11:23:26 -0300

    exercicio 02

--- aula02/src/main/java/CompraAutomovel.java
+++ aula02/src/main/java/CompraAutomovel.java
@@ -1,2 +1,24 @@
-package PACKAGE_NAME;public class CompraAutomovel {
+package org.example;
+
+import java.util.Scanner;
+
+public class CompraAutomovel {
+    public static void main(String[] args) {
+
+        Scanner scanner = new Scanner(System.in);
+
+        System.out.println("Qual sua idade? ");
+        int idade = scanner.nextInt();
+
+        System.out.println("Digite o valor do seu salário.");
+        double salario = scanner.nextDouble();
+
+        if (idade >= 18 && salario >= 2000.0) {
+            System.out.println("Você pode comprar um automóvel!");
+        } else {
+            System.out.println("Você não pode comprar um automóvel.");
+        }
+
+        scanner.close();
+    }
 }
