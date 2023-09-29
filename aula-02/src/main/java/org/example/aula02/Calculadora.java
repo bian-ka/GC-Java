1eb1ec5179c60c5ee4e4fe60a914b7ead11c866b
Bianca Gomes <gomes18bianca@gmail.com>
2023-09-29 19:14:31 -0300

    exercicio 04 aula-02

---
+++ aula-02/src/main/java/org/example/aula02/Calculadora.java
@@ -0,0 +1,49 @@
+package org.example.aula02;
+import java.util.Scanner;
+
+public class Calculadora {
+
+    public static void main(String[] args) {
+
+        Scanner input = new Scanner(System.in);
+
+        int operacao;
+        double resultado = 0;
+
+        System.out.println("Digite o primeiro número: ");
+        double num1 = input.nextDouble();
+
+        System.out.println("Digite o segundo número: ");
+        double num2 = input.nextDouble();
+
+        System.out.println("Digite a operação desejada");
+        System.out.println("1. Soma");
+        System.out.println("2. Subtração");
+        System.out.println("3. Multiplicação");
+        System.out.println("4. Divisão");
+        System.out.println("Operação: ");
+        operacao = input.nextShort();
+
+        switch (operacao) {
+            case 1:
+               resultado  = num1 + num2;
+                break;
+            case 2:
+                resultado = num1 - num2;
+                break;
+            case 3:
+                resultado = num1 * num2;
+                break;
+            case 4:
+                if (num2 == 0) {
+                    System.out.println("ERRO: Divisor não pode ser zero. Tente novamente.");
+                    break;
+                }
+                resultado = num1 / num2;
+                break;
+
+        }
+
+        System.out.println("O resultado da operação é: " + resultado);
+    }
+}
