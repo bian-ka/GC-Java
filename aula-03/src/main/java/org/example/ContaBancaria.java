8b5123e8d0f75a97191fc6c09ccf98a4b5cfd6f4
Bianca Gomes <gomes18bianca@gmail.com>
2023-10-01 13:00:20 -0300

    exercicio 01 aula-03

--- aula-03/src/main/java/org/example/ContaBancaria.java
+++ aula-03/src/main/java/org/example/ContaBancaria.java
@@ -5,57 +5,85 @@ import java.time.LocalTime;
 
 public class ContaBancaria {
 
-    String nome;
+    public String nome;
     private String cpf;
-    int identificadorConta;
-    String banco;
+    public String identificadorConta;
+    public String banco;
     private String endereco;
     private double saldo;
-
+    //construtor
+    public ContaBancaria(String nome, String cpf, String identificadorConta, String banco, String endereco) {
+        this.nome = nome;
+        this.cpf =cpf;
+        this.identificadorConta = identificadorConta;
+        this.banco = banco;
+        this.endereco = endereco;
+        this.saldo = 0.0;
+    }
+    //SACA
     public void saca (double valor) {
         if (this.saldo >= valor) {
-            this.saldo = saldo - valor;
-        }else {
-            System.out.println("O saque não pode ser realizado.");
+            this.saldo = saldo - valor; //saldo-= valor;
+            System.out.println("Saque de R$" + valor + " realizado com sucesso");
+        } else {
+            System.out.println("O saque não pode ser realizado pois o é saldo insuficiente");
         }
     }
-
+    //DEPOSITA
     public void deposita (double valor) {
-        this.saldo = saldo + valor;
-    }
+        if (this.saldo >= 0) {
+            this.saldo = saldo += valor; //saldo += valor;
+            System.out.println("Depósito de R$" + valor + " realizado com sucesso.");
+        } else {
+            System.out.println("Depósito não pode ser realizado. Tente novamente mais tarde.");
+        }
 
-    public double pix (ContaBancaria destino, double valor) {
+    }
+    //PIX CONFERIR!!!
+    public double pix (double valor) {
         if(this.saldo >= valor) {
-            this.saldo = saldo - valor;
-            destino.saldo = destino.saldo + valor;
+            this.saldo = saldo -= valor; //saldo -= valor;
+            System.out.println("Tranferência PIX no valor de R$" + valor + " realizado com sucesso.");
         }else {
-            System.out.println("Não foi possível realizar a transferencia pix.");
+            System.out.println("Não foi possível realizar a transferência PIX. Tente novamente mais tarde.");
         }
         return this.saldo;
     }
-
+    //TRANSFERE
     public void transfere (ContaBancaria destino, double valor) {
         LocalTime seteHoras = LocalTime.of(19,0);
         LocalTime oitoHoras = LocalTime.of(8,0);
         if (this.saldo >= valor && LocalTime.now().isBefore(seteHoras) && LocalTime.now().isAfter(oitoHoras)) {
             this.saldo = this.saldo - valor;
-            destino.saldo = destino.saldo + valor;
-        }else {
-            System.out.println("A transferência não pode ser realizada.");
+            destino.deposita(valor);
+            System.out.println("Transferência de R$" + valor + " realizada para a conta de " + destino.getNome() + ".");
+        } else {
+            System.out.println("A transferência não pode ser realizada por falta de saldo.");
         }
     }
-
+    //VERIFICA SALDO
     public double verificaSaldo(double saldo) {
         System.out.println("O saldo atual é: " + saldo);
         return saldo;
     }
-
+    //VERIFICA HORARIO
     public void verificarHoraraio() {
         LocalTime horarioAtual = LocalTime.now();
         System.out.println("Horário atual: " + horarioAtual);
     }
-
+    //VERIFICA INFORMAÇÕES
     public void verificarInformacoes() {
-        System.out.println("Estas são as informações da conta bancária: " + nome + cpf + identificadorConta + banco + endereco);
+        System.out.println("Nome do títular: " + nome);
+        System.out.println("CPF do títular: " + cpf);
+        System.out.println("Número identificador da conta: " + identificadorConta);
+        System.out.println("Banco: " + banco);
+        System.out.println("Endereço do títular: " + endereco);
+        verificaSaldo(saldo);
+        verificarHoraraio();
+
+    }
+
+    public String getNome() {
+        return nome;
     }
 }
---
+++ aula-03/src/main/java/org/example/TestaConta.java
@@ -0,0 +1,23 @@
+package org.example;
+
+import java.util.Scanner;
+
+public class TestaConta {
+
+    public static void main(String[] args) {
+        ContaBancaria minhaConta = new ContaBancaria("Lana Del Rey", "000.000.000-00", "122-122-22", "Love Song", "California, LA");
+
+        minhaConta.deposita(250.0);
+        minhaConta.saca(50.0);
+        minhaConta.verificarInformacoes();
+
+        ContaBancaria outraConta = new ContaBancaria("Satoru Gojo", "123.456.789-10", "123-456", "Jujuso Kaisen", "Tokio, Japão");
+
+        minhaConta.transfere(outraConta, 25.0);
+
+        outraConta.deposita(200.0);
+        outraConta.transfere(minhaConta, 950.0);
+        outraConta.verificarInformacoes();
+
+    }
+}
