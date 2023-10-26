9a2ae4a8701528cd673c9b86d8696d9876c5dd96
Bianca Gomes <gomes18bianca@gmail.com>
2023-10-26 16:50:27 -0300

    Aula dia 26

--- aula-06/src/PilhaDelivros.java
+++ aula-06/src/PilhaDelivros.java
@@ -1,5 +1,11 @@
+import java.util.Stack;
+
+//exercicio 01 da aula-06
 public class PilhaDelivros {
     public static void main(String[] args) {
 
+        int numeroDeLivros = 5;
+
+        Stack<PilhaDelivros> pilha = new Stack<>();
     }
 }
