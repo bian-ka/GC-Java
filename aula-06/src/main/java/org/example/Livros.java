69f36eb6d125e2717fa70dc70d99da8fdd888b4c
Bianca Gomes <gomes18bianca@gmail.com>
2023-11-01 09:08:36 -0300

    exercicio 01 aula-06

---
+++ aula-06/src/main/java/org/example/Livros.java
@@ -0,0 +1,25 @@
+package org.example;
+
+// exercicio 01 aula-06
+public class Livros {
+    public String titulo;
+    public String categoria;
+
+    public Livros (String titulo, String categoria) {
+        this.titulo = titulo;
+        this.categoria = categoria;
+    }
+
+    @Override
+    public String toString() {
+        return titulo + categoria;
+    }
+
+    public String getCategoria() {
+        return categoria;
+    }
+
+    public String getTitulo() {
+        return titulo;
+    }
+}
---
+++ aula-06/src/main/java/org/example/PilhaDeLivros.java
@@ -0,0 +1,58 @@
+package org.example;
+import java.util.ArrayList;
+import java.util.Stack;
+
+// exercicio 01 aula-06
+public class PilhaDeLivros {
+    public static void main(String[] args) {
+
+        Stack<Livros> pilha = new Stack<>();
+
+        pilha.push(new Livros("O Pequeno Príncipe", " Literatura Infantil"));
+        pilha.push(new Livros("Uma Breve História do Tempo", " Ciência"));
+        pilha.push(new Livros("1984", " Ficção"));
+        pilha.push(new Livros("A Culpa é das Estrelas", " Romance"));
+        pilha.push(new Livros("O Códio da Vinci", " Ficção"));
+        pilha.push(new Livros("Se Eu Ficar", " Romance"));
+
+        ArrayList<String> generosExibidos = new ArrayList<>(pilha.size());
+
+        for (int i = 0; i < pilha.size(); i ++) {
+            Livros livroAtual = pilha.get(i);
+            String generoAtual = livroAtual.getCategoria();
+
+            boolean generoJaFoiExibido = false;
+            for (int j = 0; j < generosExibidos.size(); j++) {
+                if (generoAtual.equalsIgnoreCase(generosExibidos.get(j))) {
+                    generoJaFoiExibido = true;
+                    break;
+                }
+            }
+
+            if (generoJaFoiExibido) {
+                continue;
+            } else {
+                generosExibidos.add(generoAtual);
+            }
+
+            System.out.println("Gênero de " + generoAtual + ":");
+
+            for (int j = i; j < pilha.size(); j++) {
+                Livros livroParaSerExebidos = pilha.get(j);
+                if (livroParaSerExebidos.getCategoria().equalsIgnoreCase(generoAtual)) {
+                    System.out.println(
+                        "- " +
+                        livroParaSerExebidos.getTitulo()
+                    );
+                }
+            }
+        }
+
+        while(pilha.empty() == false) {
+            pilha.pop();
+        }
+
+        System.out.println(pilha);
+
+    }
+}
